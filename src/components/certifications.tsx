7f5b589d3a5120c7e8cae77c1455a4dfa912d442
Fahad <shahzada.fahad@yieldwerx.com>
2021-05-08 03:07:36 +0500

    qr code ui fix

--- src/components/certifications.tsx
+++ src/components/certifications.tsx
@@ -15,8 +15,8 @@ const Certifications = () => {
             <div className="certification-card">
               <div className="p-4 certification-card-shade">
                 <div className="d-flex align-items-start justify-content-between">
-                  <h5 className="certification-title text-bold pb-4">{item.name}</h5>
-                  <QRCode className="certification-qr-code" value={item.verificationURL} renderAs="svg" level="M" />
+                  <h5 className="certification-title text-bold pb-4 pr-3">{item.name}</h5>
+                  <QRCode className="certification-qr-code" value={item.verificationURL} />
                 </div>
                 <div className="certification-details d-flex align-items-center justify-content-between flex-wrap">
                   <a target="_blank" href={item.verificationURL} className="certification-visit p-2 mr-2" rel="noreferrer">See Credentials</a>
