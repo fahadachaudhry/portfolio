27a0a28d734b3524bd9d3674a5caf3cf475665c4
sfahad@ssidecisions.com <sfahad@ssidecisions.com>
2022-10-14 00:24:38 +0500

    package discrepancies resolved

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -1,7 +1,10 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-use-before-define */
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import Particles from 'react-particles-js';
+import type { Container as particlesContainer, Engine } from 'tsparticles-engine';
+import Particles from 'react-particles';
+import { loadFull } from 'tsparticles';
 import avatar from '../assets/images/avatar.jpeg';
 import getProfileData from '../data/data';
 import Testimonial from './Testimonial';
@@ -15,31 +18,96 @@ const Intro = (props:IIntroProps) => {
   const { isDarkThemeEnabled } = props;
   const profileData = getProfileData();
   const themedColors = colorStore.getThemedColors(isDarkThemeEnabled);
+
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container: particlesContainer | undefined) => {
+    await console.log(container);
+  }, []);
+
   return (
     <Container fluid>
       <Row>
         <Particles
-          params={{
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={{
+            // background: {
+            //   color: {
+            //     value: '#0d47a1',
+            //   },
+            // },
+            fpsLimit: 120,
+            interactivity: {
+              events: {
+                onClick: {
+                  enable: true,
+                  mode: 'push',
+                },
+                onHover: {
+                  enable: true,
+                  mode: 'repulse',
+                },
+                resize: true,
+              },
+              modes: {
+                push: {
+                  quantity: 4,
+                },
+                repulse: {
+                  distance: 200,
+                  duration: 2,
+                },
+              },
+            },
             particles: {
               color: {
                 value: themedColors.particles,
               },
-              lineLinked: {
+              links: {
+                color: themedColors.particles,
+                distance: 150,
                 enable: true,
-                color: {
-                  value: themedColors.particles,
+                opacity: 0.3,
+                width: 1,
+              },
+              collisions: {
+                enable: true,
+              },
+              move: {
+                direction: 'none',
+                enable: true,
+                outModes: {
+                  default: 'bounce',
                 },
+                random: false,
+                speed: 2,
+                straight: false,
               },
               number: {
-                value: 70,
+                density: {
+                  enable: true,
+                  area: 800,
+                },
+                value: 80,
+              },
+              opacity: {
+                value: 0.3,
+              },
+              shape: {
+                type: 'circle',
               },
               size: {
-                value: 3,
+                value: { min: 1, max: 5 },
               },
             },
+            detectRetina: true,
           }}
-          className="position-absolute w-100 h-100 d-none d-lg-block"
         />
+
         <Col>
           <Container className="height-100vh d-flex align-items-center intro pt-4 pb-4 position-relative">
 
